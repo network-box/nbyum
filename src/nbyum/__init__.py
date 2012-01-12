da5061cfdc93d7969d182a586919b5911ff8b6b6
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2012-01-12 10:14:22 +0000

    Shut yum up
    Our output matters. Programs will read it and parse it. This is in fact
    the sole purpose of nbyum: print a parser-friendly and stable output, so
    that they don't have to screen-scrape the yum console output.
    
    As such, we don't want yum to print random stuff. So shut up yum!
    
    git-svn-id: https://10.8.18.16/nbrs/5.0/apps/nbyum/HEAD@20302 ef222f69-f2fc-0310-9845-a72751d35017

--- src/nbyum/__init__.py
+++ src/nbyum/__init__.py
@@ -1,2 +1,12 @@
+import yum
+
+
 class NBYum(object):
-    pass
+    def __init__(self):
+        self.yum = yum.YumBase()
+
+        # Shut yum up
+        self.yum.preconf.debuglevel = 0
+        self.yum.preconf.errorlevel = 0
+
+        self.yum.setCacheDir()
