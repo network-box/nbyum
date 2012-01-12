52bae505ad931f1a01954fa6d607327f0fc12b1a
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2012-01-12 11:14:43 +0000

    Fix setup.py after the rename of README.rst
    git-svn-id: https://10.8.18.16/nbrs/5.0/apps/nbyum/HEAD@20315 ef222f69-f2fc-0310-9845-a72751d35017

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if sys.version_info < (2, 7):
 
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, 'README.rst')) as f:
     README = f.read()
 
 
