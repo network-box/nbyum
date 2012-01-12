f1ce28867d3e8147c1ddb07bd2a539a21f938248
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2012-01-12 10:14:17 +0000

    Initial package skeleton and setup.py
    git-svn-id: https://10.8.18.16/nbrs/5.0/apps/nbyum/HEAD@20300 ef222f69-f2fc-0310-9845-a72751d35017

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+
+from distutils.core import setup
+
+
+requires = [
+    # Not available from Pypi, but worth mentioning for packagers
+    #'yum',
+    ]
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+
+
+setup(name='nbyum',
+      version='0.0.1',
+      description='Just like yum, but with a usable output',
+      long_description=README,
+      author='Mathieu Bridon',
+      author_email='mathieu.bridon@network-box.com',
+      license='GPLv3+',
+      url="https://www.network-box.com",
+      package_dir={'': 'src'},
+      packages=['nbyum'],
+      scripts=['nbyum'],
+      requires=requires,
+      )
