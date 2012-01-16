3046c720971f6d24574289d220deaf0f3a889ae7
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2012-01-16 09:42:25 +0000

    check-update: Drastically simplify our code
    By chance, reading the Yum API before going to bed, I discovered that
    handy update() method.
    
    Let's use it then, instead of doing it our own (wrong) way.
    
    git-svn-id: https://10.8.18.16/nbrs/5.0/apps/nbyum/HEAD@20348 ef222f69-f2fc-0310-9845-a72751d35017

--- src/nbyum/yumbase.py
+++ src/nbyum/yumbase.py
@@ -15,33 +15,11 @@ obsolete_tmpl = "{'obsolete': ('%s', '%s')}"
 class NBYumBase(yum.YumBase):
     def update_packages(self, packages, apply=False):
         """Check for updates and optionally apply."""
-        updates = self.up
-
-        if updates is None:
-            # we're _only_ called after updates are setup
-            # TODO: is that really an error case?
-            raise WTFException("Not sure what happened here, ask Mathieu :-/")
-
-        # Get packages to be updated
-        for (new, old) in updates.getUpdatesTuples():
-            if not old.name in packages:
-                continue
-
-            updating = self.getPackageObject(new)
-            updated = self.rpmdb.searchPkgTuple(old)[0]
-
-            self.tsInfo.addUpdate(updating, updated)
-
-        # Get packages to be obsoleted
-        for (obs, inst) in updates.getObsoletesTuples():
-            if not inst.name in packages:
-                continue
-
-            obsoleting = self.getPackageObject(obs)
-            installed = self.rpmdb.searchPkgTuple(inst)[0]
-
-            self.tsInfo.addObsoleting(obsoleting, installed)
-            self.tsInfo.addObsoleted(installed, obsoleting)
+        if packages:
+            for package in packages:
+                self.update(pattern=package)
+        else:
+            self.update()
 
         # Get new packages to be installed as dependencies
         res, resmsg = self.buildTransaction()
