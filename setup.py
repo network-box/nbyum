4be9b73d8151d208dc8f81d1eff0eb5e5832353e
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2012-01-16 09:41:47 +0000

    tests: Add a `test` command to the setup.py
    /me wishes he could write nbyum with Python >= 3.2 :(
    
    git-svn-id: https://10.8.18.16/nbrs/5.0/apps/nbyum/HEAD@20338 ef222f69-f2fc-0310-9845-a72751d35017

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
+import glob
 import os
+import unittest
 import sys
 
-from distutils.core import setup
+from distutils.core import setup, Command
 
 
 requires = [
@@ -18,6 +20,31 @@ with open(os.path.join(here, "README.rst")) as f:
     README = f.read()
 
 
+class TestCommand(Command):
+    description = "Run the unit tests for the module"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def __get_testfiles(self):
+        for t in glob.iglob(os.path.join("tests", "*.py")):
+            basename = os.path.basename(t)
+
+            if basename != "__init__.py__":
+                yield "tests.%s" % os.path.splitext(basename)[0]
+
+    def run(self):
+        testfiles = []
+
+        tests = unittest.TestLoader().loadTestsFromNames(self.__get_testfiles())
+        t = unittest.TextTestRunner(verbosity=1)
+        t.run(tests)
+
+
 setup(name="nbyum",
       version="0.0.1",
       description="Just like yum, but with a usable output",
@@ -30,4 +57,5 @@ setup(name="nbyum",
       packages=["nbyum"],
       scripts=["nbyum"],
       requires=requires,
+      cmdclass={'test': TestCommand},
       )
