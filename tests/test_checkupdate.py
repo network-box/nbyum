829fc23bf40a402438aa28305050c8cef0f62496
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2012-01-16 09:42:06 +0000

    tests: Run `check-update` with one update available
    This unit test makes sure that nbyum reports the update in the expected
    format.
    
    git-svn-id: https://10.8.18.16/nbrs/5.0/apps/nbyum/HEAD@20344 ef222f69-f2fc-0310-9845-a72751d35017

--- tests/test_checkupdate.py
+++ tests/test_checkupdate.py
@@ -13,3 +13,13 @@ class TestCheckUpdate(TestCase):
 
         result = [eval(line) for line in self.new_stdout.getvalue().split("\n") if line]
         self.assertEqual(result, expected)
+
+    def test_only_updates(self):
+        """Check a repo with only updates available."""
+        args = self.parser.parse_args([self.command])
+        self._run_nbyum_test(args)
+
+        expected = [{'update': ('0:foo-1-1.nb5.0.noarch', '0:foo-1-2.nb5.0.noarch')}]
+
+        result = [eval(line) for line in self.new_stdout.getvalue().split("\n") if line]
+        self.assertEqual(result, expected)
