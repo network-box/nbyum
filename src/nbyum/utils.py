e57f26851fbd7eab191d567eafba5c67b9b5e0ab
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2012-01-16 09:41:44 +0000

    utils: Move the args parser from the main script to utils
    We'll reuse it for the unit tests, to run things easily.
    
    git-svn-id: https://10.8.18.16/nbrs/5.0/apps/nbyum/HEAD@20337 ef222f69-f2fc-0310-9845-a72751d35017

--- src/nbyum/utils.py
+++ src/nbyum/utils.py
@@ -1,3 +1,40 @@
+import argparse
+
+
+def get_parser():
+    """Get the argument parser for the main nbyum command line tool."""
+    # -- Root level arguments (-h/--help is added by default) ----------------
+    parser = argparse.ArgumentParser(description="Just like yum, " \
+                                                 "but with a usable output")
+    parser.add_argument("-d", "--debug", action="store_true",
+                        help="Print some useful debug information")
+
+    subparsers = parser.add_subparsers(title="subcommands")
+
+    # -- Subcommand: check-update --------------------------------------------
+    parser_checkupdate = subparsers.add_parser("check-update",
+                                               help="Check for updates to " \
+                                                    "installed packages")
+    parser_checkupdate.add_argument("packages", nargs="*", metavar="PACKAGE",
+                                    help="The package(s) for which to check " \
+                                         "if an update is available. An " \
+                                         "arbitrary number can be specified." \
+                                         " If none is, updates for the whole" \
+                                         " system are checked.")
+    parser_checkupdate.set_defaults(func="check_update")
+
+    # -- Subcommand: update ---------------------------------------------------
+    parser_update = subparsers.add_parser("update",
+                                          help="Update packages or the " \
+                                               "whole system")
+    parser_update.add_argument("packages", nargs="*", metavar="PACKAGE",
+                               help="The package(s) to update. An arbitrary " \
+                                    "number can be specified. If none is, " \
+                                    "the whole system is updated.")
+    parser_update.set_defaults(func="update")
+
+    return parser
+
 def get_envra(pkg):
     """Get the Epoch:Name-Version-Release.Arch representation of a package."""
     return "%s:%s-%s-%s.%s" % (pkg.epoch, pkg.name, pkg.version,
