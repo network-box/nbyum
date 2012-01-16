9cf780f8771d40cfce038bdeec13982198c1cddd
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2012-01-16 09:41:58 +0000

    tests: Mother of all our test cases
    This commit introduces a TestCase class that all of our test cases can
    inherit from.
    
    It takes care of the setup needed by all test cases:
      - write the yum configs
      - get the yum repos
      - populate the yum install root
      - clean all that up after the test
    
    It also provides a handy method to run nbyum, so that test cases will be
    pretty trivial to write.
    
    git-svn-id: https://10.8.18.16/nbrs/5.0/apps/nbyum/HEAD@20342 ef222f69-f2fc-0310-9845-a72751d35017

---
+++ tests/__init__.py
@@ -0,0 +1,90 @@
+import cStringIO
+import os
+import unittest
+import subprocess
+import sys
+
+
+global_dataroot = os.path.join(os.path.abspath(os.getcwd()), "tests/data")
+conf_template = os.path.join("/etc/yum.conf")
+setuprepo_baseurl = os.path.join(global_dataroot, "setup.repo")
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        # -- We will need a parser ---------------------------------
+        from nbyum.utils import get_parser
+        self.parser = get_parser()
+
+        # -- A few useful definitions ------------------------------
+        self.dataroot = os.path.join(global_dataroot, self.command)
+        self.yumconf = os.path.join(self.dataroot,
+                                    "%s.conf" % self._testMethodName)
+        self.installroot = os.path.join(self.dataroot,
+                                        "%s.root" % self._testMethodName)
+        self.reposdir = os.path.join(self.dataroot,
+                                     "%s.repos.d" % self._testMethodName)
+        self.cachedir = os.path.join(self.dataroot,
+                                "%s.cache" % self._testMethodName)
+
+        testrepo_conf = os.path.join(self.reposdir, "test.repo")
+        testrepo_baseurl = os.path.join(self.dataroot,
+                                        "%s.repo" % self._testMethodName)
+
+        # -- Set up the yum config and repos -----------------------
+        if not os.path.isdir(self.reposdir):
+            os.makedirs(self.reposdir)
+
+        with open(conf_template, "r") as input:
+            with open(self.yumconf, "w") as output:
+                for line in input.readlines():
+                    if line.startswith("cachedir="):
+                        output.write("cachedir=%s\n" % self.cachedir)
+                    else:
+                        output.write(line)
+
+                output.write("installroot=%s\n" % self.installroot)
+                output.write("reposdir=%s\n" % self.reposdir)
+                output.write("\n")
+                output.write("[setup]\n")
+                output.write("name=Setup test repo\n")
+                output.write("enabled=1\n")
+                output.write("baseurl=file://%s\n" % setuprepo_baseurl)
+                output.write("gpgcheck=0\n")
+
+        # -- Install a handful of packages, to test transactions ---
+        cmd = ["sudo", "yum", "install", "*", "-c", self.yumconf, "-y"]
+        subprocess.check_call(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+        with open(testrepo_conf, "w") as f:
+            f.write("[test]\n")
+            f.write("name=Dummy test repo\n")
+            f.write("enabled=1\n")
+            f.write("baseurl=file://%s\n" % testrepo_baseurl)
+            f.write("gpgcheck=0\n")
+
+    def tearDown(self):
+        cmd = ["yum", "clean", "all", "-c", self.yumconf]
+        subprocess.check_call(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+        # -- Cleanup, with super user permissions ------------------
+        cmd = ["sudo", "rm", "-fr", self.yumconf,
+                                    self.installroot,
+                                    self.reposdir,
+                                    self.cachedir,
+               ]
+        subprocess.check_call(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    def _run_nbyum_test(self, args):
+        """This is not a test method, just a helper to avoid duplication."""
+        # All nbyum does is print its output, so let's capture it
+        self.old_stdout = sys.stdout
+        self.new_stdout = cStringIO.StringIO()
+        sys.stdout = self.new_stdout
+
+        from nbyum import NBYumCli
+        yummy = NBYumCli(args, yumconf=self.yumconf)
+        yummy.run()
+
+        # And restore stdout now that we finished
+        sys.stdout = self.old_stdout
