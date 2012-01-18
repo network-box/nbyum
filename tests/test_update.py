589a573f3fe3d87f7cb3c1f9ffb343348c07edc5
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2012-01-18 03:15:22 +0000

    update: Proper unit testing for the command
    The unit tests are basically copy-pasted from the check-update ones,
    with the following changes:
    - most of them are skipped if not run as root (as only root can run
      updates)
    - each test also check that packages have properly been updated
    
    As for the repos/packages, it's all hardlinking from check-update to
    save some space. :)
    
    git-svn-id: https://10.8.18.16/nbrs/5.0/apps/nbyum/HEAD@20368 ef222f69-f2fc-0310-9845-a72751d35017

---
+++ tests/test_update.py
@@ -0,0 +1,192 @@
+from operator import attrgetter
+import os
+
+from nose.exc import SkipTest
+
+import rpm
+
+from tests import TestCase
+
+
+class TestUpdate(TestCase):
+    command = "update"
+    installonlypkgs = "bar"
+
+    def _get_installed_rpms(self):
+        """Not a test, just a handy helper.
+
+        This returns the list of installed packages, to compare with what was
+        expected after the update.
+        """
+        result = []
+
+        for h in sorted(rpm.TransactionSet(self.installroot).dbMatch(),
+                        key=attrgetter("name")): 
+            if h["epoch"] is None:
+                h["epoch"] = 0
+            result.append("%(epoch)s:%(name)s-%(version)s-%(release)s.%(arch)s" % h) 
+
+        return result
+
+    def test_no_updates(self):
+        """Update from a repo with no available updates."""
+        args = self.parser.parse_args([self.command])
+        self._run_nbyum_test(args)
+
+        # -- Check the update summary ------------------------------
+        expected = []
+
+        result = [eval(line) for line in self.new_stdout.getvalue().split("\n") if line]
+        self.assertEqual(result, expected)
+
+        # -- Check the installed packages after the update ---------
+        expected = ["0:bar-1-1.nb5.0.noarch",
+                    "0:foo-1-1.nb5.0.noarch",
+                    "0:toto-1-1.nb5.0.noarch"]
+
+        result = self._get_installed_rpms()
+        self.assertEqual(result, expected)
+
+    def test_only_updates(self):
+        """Update from a repo with only updates available."""
+        if os.getuid() != 0:
+           raise SkipTest("Updates must be run as root")
+
+        args = self.parser.parse_args([self.command])
+        self._run_nbyum_test(args)
+
+        # -- Check the update summary ------------------------------
+        expected = [{'update': ('0:foo-1-1.nb5.0.noarch', '0:foo-1-2.nb5.0.noarch')}]
+
+        result = [eval(line) for line in self.new_stdout.getvalue().split("\n") if line]
+        self.assertEqual(result, expected)
+
+        # -- Check the installed packages after the update ---------
+        expected = ["0:bar-1-1.nb5.0.noarch",
+                    "0:foo-1-2.nb5.0.noarch",
+                    "0:toto-1-1.nb5.0.noarch"]
+
+        result = self._get_installed_rpms()
+        self.assertEqual(result, expected)
+
+    def test_only_install(self):
+        """Update from a repo with only new installs available."""
+        if os.getuid() != 0:
+           raise SkipTest("Updates must be run as root")
+
+        args = self.parser.parse_args([self.command])
+        self._run_nbyum_test(args)
+
+        # -- Check the update summary ------------------------------
+        expected = [{'install': '0:bar-1-2.nb5.0.noarch'}]
+
+        result = [eval(line) for line in self.new_stdout.getvalue().split("\n") if line]
+        self.assertEqual(result, expected)
+
+        # -- Check the installed packages after the update ---------
+        expected = ["0:bar-1-1.nb5.0.noarch",
+                    "0:bar-1-2.nb5.0.noarch",
+                    "0:foo-1-1.nb5.0.noarch",
+                    "0:toto-1-1.nb5.0.noarch"]
+
+        result = self._get_installed_rpms()
+        self.assertEqual(result, expected)
+
+    def test_only_obsoletes(self):
+        """Update from a repo with only obsoletes available."""
+        if os.getuid() != 0:
+           raise SkipTest("Updates must be run as root")
+
+        args = self.parser.parse_args([self.command])
+        self._run_nbyum_test(args)
+
+        # -- Check the update summary ------------------------------
+        expected = [{'obsolete': ('0:bar-1-1.nb5.0.noarch', '0:baz-2-1.nb5.0.noarch')}]
+
+        result = [eval(line) for line in self.new_stdout.getvalue().split("\n") if line]
+        self.assertEqual(result, expected)
+
+        # -- Check the installed packages after the update ---------
+        expected = ["0:baz-2-1.nb5.0.noarch",
+                    "0:foo-1-2.nb5.0.noarch",
+                    "0:toto-1-1.nb5.0.noarch"]
+
+        result = self._get_installed_rpms()
+        self.assertEqual(result, expected)
+
+    def test_install_as_dep(self):
+        """Update from a repo with an update requiring a new install."""
+        if os.getuid() != 0:
+           raise SkipTest("Updates must be run as root")
+
+        args = self.parser.parse_args([self.command])
+        self._run_nbyum_test(args)
+
+        # -- Check the update summary ------------------------------
+        expected = [{'update': ('0:toto-1-1.nb5.0.noarch', '0:toto-2-1.nb5.0.noarch')},
+                    {'installdep': '0:plouf-2-1.nb5.0.noarch'}]
+
+        result = [eval(line) for line in self.new_stdout.getvalue().split("\n") if line]
+        self.assertEqual(result, expected)
+
+        # -- Check the installed packages after the update ---------
+        expected = ["0:bar-1-1.nb5.0.noarch",
+                    "0:foo-1-2.nb5.0.noarch",
+                    "0:plouf-2-1.nb5.0.noarch",
+                    "0:toto-2-1.nb5.0.noarch"]
+
+        result = self._get_installed_rpms()
+        self.assertEqual(result, expected)
+
+    def test_ordering(self):
+        """Update from a repo with a bit of everything available."""
+        if os.getuid() != 0:
+           raise SkipTest("Updates must be run as root")
+
+        args = self.parser.parse_args([self.command])
+        self._run_nbyum_test(args)
+
+        # -- Check the update summary ------------------------------
+        expected = [{'obsolete': ('0:bar-1-1.nb5.0.noarch', '0:baz-2-1.nb5.0.noarch')},
+                    {'update': ('0:toto-1-1.nb5.0.noarch', '0:toto-2-1.nb5.0.noarch')},
+                    {'update': ('0:foo-1-1.nb5.0.noarch', '0:foo-1-2.nb5.0.noarch')},
+                    {'installdep': '0:plouf-2-1.nb5.0.noarch'}]
+
+        result = [eval(line) for line in self.new_stdout.getvalue().split("\n") if line]
+        self.assertEqual(result, expected)
+
+        # -- Check the installed packages after the update ---------
+        expected = ["0:baz-2-1.nb5.0.noarch",
+                    "0:foo-1-2.nb5.0.noarch",
+                    "0:plouf-2-1.nb5.0.noarch",
+                    "0:toto-2-1.nb5.0.noarch"]
+
+        result = self._get_installed_rpms()
+        self.assertEqual(result, expected)
+
+    def test_ordering_bis(self):
+        """Update from a repo with a bit of everything available, bis."""
+        if os.getuid() != 0:
+           raise SkipTest("Updates must be run as root")
+
+        args = self.parser.parse_args([self.command])
+        self._run_nbyum_test(args)
+
+        # -- Check the update summary ------------------------------
+        expected = [{'install': '0:bar-1-2.nb5.0.noarch'},
+                    {'update': ('0:toto-1-1.nb5.0.noarch', '0:toto-2-1.nb5.0.noarch')},
+                    {'update': ('0:foo-1-1.nb5.0.noarch', '0:foo-1-2.nb5.0.noarch')},
+                    {'installdep': '0:plouf-2-1.nb5.0.noarch'}]
+
+        result = [eval(line) for line in self.new_stdout.getvalue().split("\n") if line]
+        self.assertEqual(result, expected)
+
+        # -- Check the installed packages after the update ---------
+        expected = ["0:bar-1-1.nb5.0.noarch",
+                    "0:bar-1-2.nb5.0.noarch",
+                    "0:foo-1-2.nb5.0.noarch",
+                    "0:plouf-2-1.nb5.0.noarch",
+                    "0:toto-2-1.nb5.0.noarch"]
+
+        result = self._get_installed_rpms()
+        self.assertEqual(result, expected)
