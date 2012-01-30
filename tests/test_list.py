8d9d2f8d619f4304c4ba72f6861b1a71de8a153c
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2012-01-30 10:31:15 +0000

    list: Some unit tests for listing security modules
    git-svn-id: https://10.8.18.16/nbrs/5.0/apps/nbyum/HEAD@20445 ef222f69-f2fc-0310-9845-a72751d35017

---
+++ tests/test_list.py
@@ -0,0 +1,39 @@
+from tests import TestCase
+
+
+class TestListSMs(TestCase):
+    command = "list"
+
+    def test_list_installed_sms(self):
+        """List installed sms."""
+        args = [self.command, "installed", "sms"]
+        self._run_nbyum_test(args)
+
+        # -- Check the listing -------------------------------------
+        expected = [{'installed': '0:nbsm-foo-1-1.nb5.0.noarch'}]
+
+        result = [eval(line) for line in self.stdout.split("\n") if line]
+        self.assertEqual(result, expected)
+
+    def test_list_available_sms(self):
+        """List available sms."""
+        args = [self.command, "available", "sms"]
+        self._run_nbyum_test(args)
+
+        # -- Check the listing -------------------------------------
+        expected = [{'available': '0:nbsm-bar-1-1.nb5.0.noarch'}]
+
+        result = [eval(line) for line in self.stdout.split("\n") if line]
+        self.assertEqual(result, expected)
+
+    def test_list_sms(self):
+        """List all sms."""
+        args = [self.command, "all", "sms"]
+        self._run_nbyum_test(args)
+
+        # -- Check the listing -------------------------------------
+        expected = [{'installed': '0:nbsm-foo-1-1.nb5.0.noarch'},
+                    {'available': '0:nbsm-bar-1-1.nb5.0.noarch'}]
+
+        result = [eval(line) for line in self.stdout.split("\n") if line]
+        self.assertEqual(result, expected)
