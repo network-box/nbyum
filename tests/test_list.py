39e98a5c764e27a16df4f4bb4b195f0311eefb90
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2012-01-30 10:31:21 +0000

    list: Moar unit tests, to exerce patterns matching
    git-svn-id: https://10.8.18.16/nbrs/5.0/apps/nbyum/HEAD@20446 ef222f69-f2fc-0310-9845-a72751d35017

--- tests/test_list.py
+++ tests/test_list.py
@@ -37,3 +37,27 @@ class TestListSMs(TestCase):
 
         result = [eval(line) for line in self.stdout.split("\n") if line]
         self.assertEqual(result, expected)
+
+    def test_list_no_sms_match(self):
+        """Make sure no sms are returned with a bogus pattern."""
+        args = [self.command, "all", "sms", "no_such_sm"]
+        self._run_nbyum_test(args)
+
+        # -- Check the listing -------------------------------------
+        expected = []
+
+        result = [eval(line) for line in self.stdout.split("\n") if line]
+        self.assertEqual(result, expected)
+
+    def test_list_sms_match(self):
+        """List only matching sms."""
+        args = [self.command, "all", "sms", "nbsm-foo", "*a*"]
+        self._run_nbyum_test(args)
+
+        # -- Check the listing -------------------------------------
+        expected = [{'installed': '0:nbsm-foo-1-1.nb5.0.noarch'},
+                    {'available': '0:nbsm-bar-1-1.nb5.0.noarch'}]
+
+        result = [eval(line) for line in self.stdout.split("\n") if line]
+        print result
+        self.assertEqual(result, expected)
