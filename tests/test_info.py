91e50bb34e638dcbe45c491083680ad9336d4127
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2012-02-07 10:38:23 +0000

    info: Check apostrophes in package details
    This is actually what prompted me to use json.dumps() everywhere, as I
    was having trouble properly escaping the apostrophes in our output.
    json.dumps() does that for us, in a way that is compliant with the JSON
    specification, so that the caller will always be able to parse it.
    
    git-svn-id: https://10.8.18.16/nbrs/5.0/apps/nbyum/HEAD@20567 ef222f69-f2fc-0310-9845-a72751d35017

--- tests/test_info.py
+++ tests/test_info.py
@@ -58,3 +58,20 @@ class TestInfo(TestCase):
 
         result = [json.loads(line) for line in self.stdout.split("\n") if line]
         self.assertEqual(result, expected)
+
+    def test_info_apostrophes(self):
+        """Print the infos, including apostrophes."""
+        args = [self.command, "bdummy"]
+        self._run_nbyum_test(args)
+
+        # -- Check the infos ---------------------------------------
+        expected = [{'name': 'bdummy', 'base_package_name': 'bdummy', 'license': 'MIT',
+                     'epoch': '0', 'version': '1', 'release': '1.nb5.0',
+                     'arch': 'noarch', 'summary': 'Not just any dummy',
+                     'description': 'This package provides bochecha\'s ' \
+                                    'dummy.\n\nThe main "feature" is to make' \
+                                    ' it\'s alright to have apostrophes in a' \
+                                    ' field, so\nlet\'s add one more :\'}"'}]
+
+        result = [json.loads(line) for line in self.stdout.split("\n") if line]
+        self.assertEqual(result, expected)
