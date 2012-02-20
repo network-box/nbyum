b9a6e156854fddb48ff79af714849e45c4c28f4e
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2012-02-20 03:42:56 +0000

    install: Test the security module installation
    git-svn-id: https://10.8.18.16/nbrs/5.0/apps/nbyum/HEAD@20653 ef222f69-f2fc-0310-9845-a72751d35017

---
+++ tests/test_install_sms.py
@@ -0,0 +1,107 @@
+import os
+import unittest2
+
+from tests import TestCase
+
+
+class TestInstallSms(TestCase):
+    command = "install"
+
+    @unittest2.skipIf(os.getuid() != 0, "Installations must be run as root")
+    def test_no_sm_match(self):
+        """Try installing an unexisting security module."""
+        args = [self.command, "sms", "no_such_security_module"]
+
+        # -- Check the error message -------------------------------
+        expected = [{"error": "No package(s) available to install"}]
+        self._run_nbyum_test(args, expected)
+
+        # -- Check the installed packages after the no-op ----------
+        expected = ["0:bar-1-1.nb5.0.noarch",
+                    "0:foo-1-1.nb5.0.noarch",
+                    "0:nbsm-foo-1-1.nb5.0.noarch",
+                    "0:toto-1-1.nb5.0.noarch"]
+
+        result = self._get_installed_rpms()
+        self.assertEqual(result, expected)
+
+    @unittest2.skipIf(os.getuid() != 0, "Installations must be run as root")
+    def test_already_installed_sm(self):
+        """Try installing an already installed security module."""
+        args = [self.command, "sms", "foo"]
+
+        # -- Check the error message -------------------------------
+        expected = [{"warning": "Package nbsm-foo-1-1.nb5.0.noarch already installed and latest version"}]
+        self._run_nbyum_test(args, expected)
+
+        # -- Check the installed packages after the no-op ----------
+        expected = ["0:bar-1-1.nb5.0.noarch",
+                    "0:foo-1-1.nb5.0.noarch",
+                    "0:nbsm-foo-1-1.nb5.0.noarch",
+                    "0:toto-1-1.nb5.0.noarch"]
+
+        result = self._get_installed_rpms()
+        self.assertEqual(result, expected)
+
+    @unittest2.skipIf(os.getuid() != 0, "Installations must be run as root")
+    def test_install_sm(self):
+        """Try installing a security module."""
+        args = [self.command, "sms", "nbsm-bar"]
+
+        # Check the installation summary ---------------------------
+        expected = [{"install": "0:nbsm-bar-1-1.nb5.0.noarch"}]
+        self._run_nbyum_test(args, expected)
+
+        # -- Check the installed packages after the install --------
+        expected = ["0:bar-1-1.nb5.0.noarch",
+                    "0:foo-1-1.nb5.0.noarch",
+                    "0:nbsm-bar-1-1.nb5.0.noarch",
+                    "0:nbsm-foo-1-1.nb5.0.noarch",
+                    "0:toto-1-1.nb5.0.noarch"]
+
+        result = self._get_installed_rpms()
+        self.assertEqual(result, expected)
+
+    @unittest2.skipIf(os.getuid() != 0, "Installations must be run as root")
+    def test_install_sm_with_deps(self):
+        """Try installing a security module and its dependencies."""
+        args = [self.command, "sms", "nbsm-plouf"]
+
+        # Check the installation summary ---------------------------
+        expected = [{"install": "0:nbsm-plouf-1-1.nb5.0.noarch"},
+                    {"installdep": "0:plouf-2-1.nb5.0.noarch"}]
+        self._run_nbyum_test(args, expected)
+
+        # -- Check the installed packages after the install --------
+        expected = ["0:bar-1-1.nb5.0.noarch",
+                    "0:foo-1-1.nb5.0.noarch",
+                    "0:nbsm-foo-1-1.nb5.0.noarch",
+                    "0:nbsm-plouf-1-1.nb5.0.noarch",
+                    "0:plouf-2-1.nb5.0.noarch",
+                    "0:toto-1-1.nb5.0.noarch"]
+
+        result = self._get_installed_rpms()
+        self.assertEqual(result, expected)
+
+    @unittest2.skipIf(os.getuid() != 0, "Installations must be run as root")
+    def test_install_multiple_sms(self):
+        """Try installing several security modules and their dependencies."""
+        args = [self.command, "sms", "nbsm-plouf", "toto"]
+
+        # Check the installation summary ---------------------------
+        expected = [{"install": "0:nbsm-plouf-1-1.nb5.0.noarch"},
+                    {"install": "0:nbsm-toto-1-1.nb5.0.noarch"},
+                    {"installdep": "0:plouf-2-1.nb5.0.noarch"}]
+        self._run_nbyum_test(args, expected)
+
+        # -- Check the installed packages after the install --------
+        expected = ["0:bar-1-1.nb5.0.noarch",
+                    "0:foo-1-1.nb5.0.noarch",
+                    "0:nbsm-foo-1-1.nb5.0.noarch",
+                    "0:nbsm-plouf-1-1.nb5.0.noarch",
+                    "0:nbsm-toto-1-1.nb5.0.noarch",
+                    "0:plouf-2-1.nb5.0.noarch",
+                    "0:toto-1-1.nb5.0.noarch"]
+
+        result = self._get_installed_rpms()
+        self.assertEqual(result, expected)
