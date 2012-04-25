9fd0d2da651bdaf348aea1b673967a16797bd73f
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2012-04-25 09:01:00 +0000

    Reorganize a bit the logging code again
    This will prepare for the glorious logging future.
    
    git-svn-id: https://10.8.18.16/nbrs/5.0/apps/nbyum/HEAD@21515 ef222f69-f2fc-0310-9845-a72751d35017

--- src/nbyum/logging_hijack.py
+++ src/nbyum/logging_hijack.py
@@ -8,18 +8,18 @@ class NBYumLogger(logging.Logger):
     def handle(self, record):
         level = record.levelname.lower()
 
+        # We don't differentiate between those 3
+        if level in ("critical", "fatal"):
+            level = "error"
 
-        if level not in ("debug", "info", "warning", "error"):
-            if level in ("critical", "fatal"):
-                level = "error"
+        # Parse those multilevel from Yum into something meaningful
+        if level.startswith("debug"):
+            level = "debug"
+        if level.startswith("info"):
+            level = "info"
 
-            # Parse those multilevel from Yum into something meaningful
-            elif level.startswith("debug"):
-                level = "debug"
-            elif level.startswith("info"):
-                level = "info"
+        if level in ("debug", "info", "warning", "error"):
+            print(json.dumps({"type": level, "message": record.getMessage()}))
 
-            else:
-                raise WTFException("Got unexpected logging level: %s" % level)
-
-        print(json.dumps({level: record.getMessage()}))
+        else:
+            raise WTFException("Got unexpected logging level: %s" % level)
