6c28b7d43e18b80f4818bb630cad7f8cb25347ba
hq.mathieu.bridon <hq.mathieu.bridon@ef222f69-f2fc-0310-9845-a72751d35017>
2013-05-29 04:09:21 +0000

    New release: 5.0.7
    git-svn-id: https://10.8.18.16/nbrs/5.0/apps/nbyum/HEAD@25671 ef222f69-f2fc-0310-9845-a72751d35017

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ class InstalledTestCommand(Command):
 
 
 setup(name="nbyum",
-      version="5.0.6",
+      version="5.0.7",
       description="Just like yum, but with a usable output",
       long_description=README,
       author="Mathieu Bridon",
